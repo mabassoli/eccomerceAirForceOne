7e299fba4af0c78808c3c0c8058e3682b5fa252e
mabassoli <martinbassoli02@gmail.com>
2023-06-21 23:18:24 -0300

    sumo componente navbar, saludo y estilos al navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import NavBar from "./components/layout/navBar/navBar";
+import ItemList from "./components/page/itemList/itemList";
 
 const App = () => {
-	return <NavBar></NavBar>;
+	const [saludo, setSaludo] = useState("hola como estas");
+
+	const cambiarSaludo = (nuevoSaludo) => {
+		setSaludo(nuevoSaludo);
+	};
+
+	return (
+		<>
+			<NavBar></NavBar>
+			<ItemList saludo={saludo} cambiarSaludo={cambiarSaludo}></ItemList>
+		</>
+	);
 };
 
 export default App;
--- src/components/layout/navBar/navBar.jsx
+++ src/components/layout/navBar/navBar.jsx
@@ -1,20 +1,36 @@
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import SearchIcon from "@mui/icons-material/Search";
 import "../navBar/navBar.css";
 
 const NavBar = () => {
 	return (
 		<div className="container-navbar">
-			<img src="" alt="" />
-			<div>
-				<div className="container-navbar__items">
-					<p>Sobre Nosotros</p>
-					<p>Modelos</p>
-					<p>Contacto</p>
+			<div className="container-max-width">
+				<img src="../src/assets/logo.svg" alt="logo air force one" />
+				<div className="container-navbar__content">
+					<div className="container-navbar__content__items">
+						<ul>
+							<li>
+								<a href="#">Sobre Nosotros</a>
+							</li>
+							<li>
+								<a href="#">Modelos</a>
+							</li>
+							<li>
+								<a href="#">Contacto</a>
+							</li>
+						</ul>
+					</div>
+					<div className="searchBar">
+						<input type="search" placeholder="Buscar" />
+						<a href="">
+							<SearchIcon></SearchIcon>
+						</a>
+					</div>
+					<a href="" className="cart">
+						<ShoppingCartIcon></ShoppingCartIcon>
+					</a>
 				</div>
-				<div></div>
-				<a href="">
-					<ShoppingCartIcon></ShoppingCartIcon>
-				</a>
 			</div>
 		</div>
 	);
